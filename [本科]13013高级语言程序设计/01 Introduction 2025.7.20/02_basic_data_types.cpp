f64275cbcddba8993a2783ebf342e22697e2600b
KJZH001 <2802007619@qq.com>
2025-07-20 16:31:03 +0800

    C++的基本数据类型

---
+++ [本科]13013高级语言程序设计/01 Introduction 2025.7.20/02_basic_data_types.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std; 
+
+/*
+C++的基本数据类型
+类别         类型    Bytes   类型           Bytes
+integers    bool    1       long            4
+            char    1       unsigned int    4
+            int     4       unsigned short  2
+            short   2       unsigned long   4
+Franctional float   4       double          8
+numbers 
+
+前者为整数，后者为浮点数
+
+类型不同，消耗的bytes空间自然也就不同
+但实际上会消耗多少，这个和你使用的编译器有关，这并不是绝对的
+（大概率应该会和上面的表格相同，但就算不同也不奇怪）
+*/
+
+int main()
+{
+
+}
