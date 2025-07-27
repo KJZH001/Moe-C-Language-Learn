7b05bca0ffffdaa266cc0afbee9c020900b49b35
KJZH001 <2802007619@qq.com>
2025-07-27 14:44:35 +0800

    书本的求和例子

---
+++ [本科]13013高级语言程序设计/#02 Unit2 2025.7.27/Node4 2025.7.27/01_Calculate_product.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1,n2,result;
+    cout << "please input the two numbers:\n";
+    cin >> n1 >> n2;
+    result=n1*n2;
+    cout << "the result is :" << result << "\n";
+
+    system("pause");
+    return 0;
+}
