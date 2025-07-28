8ed0e59dd45d4bc84256db7f1e1df7bb65987800
KJZH001 <2802007619@qq.com>
2025-07-28 21:04:45 +0800

    10_our_while_program

--- [本科]13013高级语言程序设计/01 Introduction 2025.7.20/10_our_while_program.cpp
+++ [本科]13013高级语言程序设计/01 Introduction 2025.7.20/10_our_while_program.cpp
@@ -5,16 +5,16 @@ int main()
 {
     // 这是由08_our_3rd_cpp_program修改而来
     // 在这个项目中，我们将会开始使用while
+
     int num1=0, num2=0;
     cout << "Please enter two numbers, sparated by a white space : ";
     cin >> num1 >> num2;
-
-    if(num1 > num2)
-        cout << "The larger one is " << num1;
-    if(num1 < num2)
-        cout << "The larger one is " << num2;
-    if(num1 == num2)
-        cout << "The two are equal";
+    // 可以把while当成是一个会回来检查的if
+    while(num1 > num2)
+    {
+        cout << "number 1 is " << num1 << "\n";
+        num1 = num1 - 1;
+    }
     
     cout << "\n";
 
---
+++ [本科]13013高级语言程序设计/01 Introduction 2025.7.20/11_erroes.cpp
@@ -0,0 +1,6 @@
+/*
+错误主要分为两种
+Syntax error 语法错误
+logic errors 逻辑错误
+
+*/
