aa5b24dad4a4765ae2f80d68fab1733e207e8aa6
KJZH001 <2802007619@qq.com>
2025-08-03 15:02:23 +0800

    if-else使用

---
+++ [本科]13013高级语言程序设计/02 Selection and Repetition 2025.7.28/02_if_else_example.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int income=0,tax=0;
+    cout << "please enter your income :";
+    cin >> income;
+
+    if(income <= 10000)
+    {
+        tax=0.02*income;
+    }
+    else
+    {
+        tax=0.08*(income-10000) +200;
+    }
+
+    cout << "Tax amount: $" << tax << "\n";
+
+    system("pause");
+}
