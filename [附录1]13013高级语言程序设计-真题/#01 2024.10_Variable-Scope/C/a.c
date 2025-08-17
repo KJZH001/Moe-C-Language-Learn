7b2ee977eb14789d3a6741595b069c475194e273
KJZH001 <2802007619@qq.com>
2025-08-17 11:43:48 +0800

    补充提交

--- [附录1]13013高级语言程序设计-真题/#01 2024.10_Variable-Scope/C/a.c
+++ [附录1]13013高级语言程序设计-真题/#01 2024.10_Variable-Scope/C/a.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int count = 5;  // 在 a.c 中定义 count
 
 void show_count() {
     printf("Count: %d\n", count);
+    system("pause");
 }
--- [附录1]13013高级语言程序设计-真题/#01 2024.10_Variable-Scope/C/b.c
+++ [附录1]13013高级语言程序设计-真题/#01 2024.10_Variable-Scope/C/b.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "a.c"
 
 extern int count;  // 声明 count 变量，告诉编译器 count 是在其他文件中定义的
 
 int main() {
     printf("Count from b.c: %d\n", count);
+    system("pause");
     return 0;
 }
