7eb53efb982133566b4948dc32c0dc3e08b48264
KJZH001 <2802007619@qq.com>
2025-09-03 22:14:02 +0800

    指针有两种写法

--- [本科]13013高级语言程序设计/08 Pointers 2025.09.03/01_point-have-a-try.cpp
+++ [本科]13013高级语言程序设计/08 Pointers 2025.09.03/01_point-have-a-try.cpp
@@ -1,3 +1,4 @@
+// 来尝试我们的第一个指针项目
 #include <iostream>
 using namespace std;
 
@@ -10,10 +11,21 @@ int main()
 
     2^32 byte=4GiB，这也是为何32位电脑最大只支持4G的记忆体（内存条）的原因
     */
-    int a =5;
-    double b=10.5;
+    
+    
+   int a =5;
+   double b=10.5;
+   /*
+   指针有两种写法
+   int *aPtr=&a; 这是一种（*写在变量名前面）
+   int* aPtr=&a; 这是另一种（*写在类型的后面）
+   两种的效果都是同样的
+   */
     int *aPtr=&a;
     double *bPtr=&b;
+    
+    // 栈堆是从高向低处分配
+    // &的意思是，取出这个变量本身的地址
 
     cout << &a << "\n";
     cout << &b << "\n";
