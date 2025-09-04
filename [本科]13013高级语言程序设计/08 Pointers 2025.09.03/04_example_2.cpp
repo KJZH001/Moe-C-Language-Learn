2b2c45df08878c21e1d7e869c883403df81fd7af
KJZH001 <2802007619@qq.com>
2025-09-04 21:03:55 +0800

    例子2

--- [本科]13013高级语言程序设计/08 Pointers 2025.09.03/04_example_2.cpp
+++ [本科]13013高级语言程序设计/08 Pointers 2025.09.03/04_example_2.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int main()
 {
     int a =10;
-    int* ptr =&a;
-    cout << *ptr << "\n"; //?
-    *ptr =5;
+    int* ptr1;
+    int* ptr2;
+    ptr1 = ptr2 = &a;
+    cout << *ptr1 << "\n"; //?
+    *ptr2 =5;
+    cout << *ptr1 << "\n"; //?
+    (*ptr1)++;
     cout << a << "\n"; //?
-    a =18;
-    cout << *ptr << "\n";
+    
 
     system("pause");
     return 0;
